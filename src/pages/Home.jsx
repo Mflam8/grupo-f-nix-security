26fb375867f764a5e05ee4d2f465248b2350b505
Marion Flamenco <mayaflamenco89@gmail.com>
2026-02-26 04:40:01 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -423,7 +423,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="services-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {services.map((s, i) => {
               const Icon = s.icon;
               return (
